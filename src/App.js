1fcbfa3219c6b8740b1506a08a33eab844b4c862
sifoa <85497710+sifoa@users.noreply.github.com>
2021-06-07 20:06:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       
-        {/* <a
+         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a> 
 
-        {/* <p>Todo List</p> */}
+         <p>Todo List</p> 
 
-        {/* <input type="text" placeholder="input todo" name="todo_input"/> */}
+         <input type="text" placeholder="input todo" name="todo_input"/> 
         {/* <ul>
           <li>Masak</li>
           <li>Mencuci</li>
           <li>Makan</li>
         </ul> */}
 
-        {/* <button role="button" name="submit">Add Todo</button> */}
+         <button role="button" name="submit">Add Todo</button> 
 
 
       </header>
